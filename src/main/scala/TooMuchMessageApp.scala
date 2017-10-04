752e166c823f5e7da31344f2fdff24f325e9c393
murosan <masashi_16n1200013@nnn.ed.jp>
2017-10-04 17:34:14 +0900

    上級

--- src/main/scala/TooMuchMessageApp.scala
+++ src/main/scala/TooMuchMessageApp.scala
@@ -1,3 +1,29 @@
-object TooMuchMessageApp extends App {
+import java.text.SimpleDateFormat
+import java.util.Date
 
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
+
+class HeavyReceiver extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case d: String =>
+      Thread.sleep(1000)
+      log.info(d)
+  }
+}
+
+object TooMuchMessageApp extends App {
+  val system = ActorSystem("tooMuchMessageApp")
+
+  val heavyReceiver = system.actorOf(Props[HeavyReceiver], "heavyReceiver")
+
+  val formatPattern = new SimpleDateFormat("yyyy年MM月dd日E曜日H時mm分ss秒")
+
+  while (true) {
+    heavyReceiver ! formatPattern.format(new Date())
+  }
+
+  system.terminate()
 }
