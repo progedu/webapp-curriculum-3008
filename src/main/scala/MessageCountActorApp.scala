aef61a828286f7599e7f634d766987dd53e7fd93
haru221 <gadget.tf@gmail.com>
2018-05-25 22:07:56 +0900

    Akka とアクターモデルの挑戦問題の解答

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,31 @@
-object MessageCountActorApp extends App {
+import java.util.concurrent.atomic.AtomicInteger
 
+import akka.actor.{Actor, ActorSystem, Props}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class CountUpActor extends Actor {
+  val counter = new AtomicInteger(0)
+
+  override def receive: Receive = {
+    case "countUp" => {
+      val count = counter.incrementAndGet()
+      println(count)
+    }
+  }
+}
+
+object MessageCountActorApp extends App {
+  val MAX_COUNT = 10000
+  val system = ActorSystem("messageCountActor")
+  val myActor = system.actorOf(Props[CountUpActor])
+
+  for (i <- 1 to MAX_COUNT) {
+    myActor ! "countUp"
+    // スリープが無いと途中で死んでしまう
+    Thread.sleep(1)
+  }
+
+  Await.ready(system.terminate(), Duration.Inf)
 }
--- src/main/scala/TooMuchMessageApp.scala
+++ src/main/scala/TooMuchMessageApp.scala
@@ -1,3 +1,33 @@
-object TooMuchMessageApp extends App {
+import java.text.SimpleDateFormat
 
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
+import java.util.Date
+
+import scala.util.Random
+
+class HeavyWorkActor extends Actor {
+  val MIN_WAIT = 1000
+  val RANDOM_MAX = 1000
+  val random = new Random()
+  val logging = Logging(context.system, this)
+
+  override def receive: Receive = {
+    case date: Date => {
+      val formatter = new SimpleDateFormat("YYYY年MM月dd日 HH時mm分ss秒")
+      logging.info(formatter.format(date))
+      // 1000ms〜2000msのwait
+      val waitTime = random.nextInt(RANDOM_MAX) + MIN_WAIT
+      Thread.sleep(waitTime)
+    }
+  }
+}
+
+object TooMuchMessageApp extends App {
+  val system = ActorSystem("tooMuchMessage")
+  val heavyActor = system.actorOf(Props[HeavyWorkActor])
+
+  while (true) {
+    heavyActor ! new Date()
+  }
 }
