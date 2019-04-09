d026c2260e24afc32feeaacd544c6b617a1005a1
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2019-04-09 16:46:01 +0900

    一つのメッセージを処理するのに 1000 秒以上かかるようなアクターを実装

--- src/main/scala/TooMuchMessageApp.scala
+++ src/main/scala/TooMuchMessageApp.scala
@@ -1,3 +1,27 @@
+import java.util.Date
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
+
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case message: String => {
+      log.info(message)
+      Thread.sleep(1000)
+    }
+    case _ =>
+  }
+}
+
 object TooMuchMessageApp extends App {
 
+  val system = ActorSystem("actorStudy")
+
+  val myActor = system.actorOf(Props[MyActor], "myActor")
+
+  while (true) {
+    myActor ! new Date().toString
+  }
 }
