75939e62f64924b4077b86af653cf0791581fec1
sdk40010 <sdk40010@gmail.com>
2019-05-12 18:18:05 +0900

    挑戦の課題

--- src/main/scala/ReplyActorApp.scala
+++ src/main/scala/ReplyActorApp.scala
@@ -1,3 +1,28 @@
-object ReplyActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
 
+class ReplyActor1 extends Actor {
+  def receive = {
+    case reply: String => {
+      println(reply)
+      sender ! "from actor1"
+    }
+  }
+}
+
+class ReplyActor2 extends Actor {
+  def receive = {
+    case reply: String => {
+      println(reply)
+      sender ! "from actor2"
+    }
+  }
+}
+
+object ReplyActorApp extends App {
+  val system = ActorSystem("replyActorApp")
+
+  val actor1 = system.actorOf(Props[ReplyActor1], "replyActor1")
+  val actor2 = system.actorOf(Props[ReplyActor2], "replyActor2")
+
+  actor2.tell("from actor1", actor1)
 }
