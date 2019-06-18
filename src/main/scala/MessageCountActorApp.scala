bc50621e1fcd83d42821224c30c43979a245f973
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2019-06-19 01:41:14 +0900

    初級・中級

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,25 @@
-object MessageCountActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
 
+case object Count
+
+class Printer extends Actor {
+  var counter: Int = 0
+
+  def receive = {
+    case Count => {
+      counter = counter + 1
+      println(s"Count: $counter")}
+  }
+}
+
+object MessageCountActorApp extends App {
+  val system = ActorSystem("messageCount")
+
+  val messageCountActor = system.actorOf(Props[Printer], "messageCountActor")
+
+  for (i <- 1 to 10000) {
+    messageCountActor ! Count
+  }
+
+  Thread.currentThread().join()
 }
--- src/main/scala/ReplyActorApp.scala
+++ src/main/scala/ReplyActorApp.scala
@@ -1,3 +1,30 @@
-object ReplyActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
 
+case object Greet
+
+class Greeter1 extends Actor {
+  def receive = {
+    case Greet => {
+      println("おい！")
+      sender ! Greet
+    }
+  }
+}
+
+class Greeter2 extends Actor {
+  def receive = {
+    case Greet => {
+      println("こら！")
+      sender ! Greet
+    }
+  }
+}
+
+object ReplyActorApp extends App {
+  val system = ActorSystem("replyActor")
+
+  val greeter1 = system.actorOf(Props[Greeter1], "greeter1")
+  val greeter2 = system.actorOf(Props[Greeter2], "greeter2")
+
+  greeter1.tell(Greet, greeter2)  // greeter1 に Greet を送信
 }
