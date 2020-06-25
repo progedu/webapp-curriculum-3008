32cad3741d11f842fc0ae9327df77e541a43e9e4
YuuTakimotoY <44459676+YuuTakimotoY@users.noreply.github.com>
2020-06-25 22:41:07 +0900

    1 から 10000 まで出力したあと、ActorSystem を終了させる

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,23 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+class MessageCountActor extends Actor {
+  var count = 0
+  def receive = {
+    case _ => {
+      count = count + 1
+      println(count)
+    }
+  }
+}
+
 object MessageCountActorApp extends App {
+  val system = ActorSystem("MessageCountActorApp")
+  val myActor = system.actorOf(Props[MessageCountActor], "messageCountActor")
+
+  for(i <- 1 to 10000){
+    myActor ! "test"
+  }
+
+  Thread.currentThread().join()
 
 }
