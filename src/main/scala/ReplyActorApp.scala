6b76f614e2eaaddbe0eee3c5d9e1c53019c6fbcc
YuuTakimotoY <44459676+YuuTakimotoY@users.noreply.github.com>
2020-06-25 22:53:12 +0900

    お互いにメッセージを返信し合いその内容をコンソールに出力し続ける 2 つのアクターを実装

--- src/main/scala/ReplyActorApp.scala
+++ src/main/scala/ReplyActorApp.scala
@@ -1,3 +1,37 @@
-object ReplyActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
 
+case object Ping
+case object Pong
+
+class PingActor extends Actor {
+  val log = Logging(context.system,this)
+
+  def receive ={
+    case Ping => {
+      log.info("ping")
+      sender ! Pong
+    }
+    case _ =>
+  }
+}
+
+class PongActor extends Actor {
+  val log = Logging(context.system,this)
+
+  def resive ={
+    case Pong => {
+      log.info("pong")
+      sender ! Ping
+    }
+    case _ =>
+  }
+}
+
+object ReplyActorApp {
+  val system = ActorSystem("ReplyActorApp")
+  val pingActor = system.actorOf(Props[PingActor], "pingActor")
+  val pongActor = system.actorOf(Props[PongActor], "pongActor")
+  
+  pingActor.tell(Ping,pongActor)
 }
