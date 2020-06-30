7e6ed4cbb014bc5e41dd64826fdd870150146021
JojiKoike <joji.koike@gmail.com>
2020-07-01 08:27:40 +0900

    メッセージが送られた回数をコンソールに出力するアクターを実装

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,26 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+class MessageCountActor extends Actor {
+  var count = 0
+
+  override def receive: Receive = {
+    case _ =>
+      count += 1
+      println(count)
+  }
+}
+
 object MessageCountActorApp extends App {
+
+  val system = ActorSystem("messageCountActorApp")
+
+  val messageCountActor =
+    system.actorOf(Props[MessageCountActor], "messageCountActor")
+
+  for (i <- 1 to 1000) {
+    messageCountActor ! "call"
+  }
+
+  Thread.currentThread().join()
 
 }
